9e1e546cc60f63b13cc552ada817d704f33ea2e0
Сергей Шапшинов <sbredman13@icloud.com>
2023-04-12 14:02:06 +0300

    deploy test

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -117,9 +117,9 @@ REST_FRAMEWORK = {
 
 AUTH_USER_MODEL = "users.User"
 
-#SIMPLE_JWT = {
-    #"ACCESS_TOKEN_LIFETIME": timedelta(days=10),
-#}
+# SIMPLE_JWT = {
+    # "ACCESS_TOKEN_LIFETIME": timedelta(days=10),
+# }
 
 DJOSER = {
     "LOGIN_FIELD": "email",
